9bc897e42e88ba7e6023170a573209e612dc849f
atfox272 <trananhtai577@gmail.com>
2025-03-24 10:55:43 +0700

    add: testbench

---
+++ sim/rtl.f
@@ -0,0 +1,6 @@
+./../rtl/*.v
+./../rtl/common/memory/*.v
+./../rtl/common/adapter/axi/*.v
+./../rtl/common/fifo/sync_fifo/*.v
+./../rtl/common/skid_buffer/*.v
+./../rtl/common/encoder/priority_encoder/*.v
